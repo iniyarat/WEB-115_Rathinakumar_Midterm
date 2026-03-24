7f3f16f942ab63bb9d34d1b9cdfa66f27e8e255a
Iniya Rathinakumar <irathinakumar@my.waketech.edu>
2026-03-24 14:06:58 -0400

    tried to finish step 3 - ran out of time

--- amortizationCalculator.js
+++ amortizationCalculator.js
@@ -10,13 +10,14 @@ document.getElementById("Button1").onclick = function() {
         term = Number(term);
         if (term != 15 && term != 30) {
             alert("Must be 15 or 30");
+            return;
         }
 
         //step 2: initial calculations
         let principal = loan - down;
         let rate = 0.0575;
         let mRate = rate/12; //because rate is annual
-        let months = terms*12;
+        let months = term*12;
 
         let payment = (mRate * principal) / (1-Math.pow(1+mRate, -months))
         let totalLoan = payment * months;
@@ -25,10 +26,15 @@ document.getElementById("Button1").onclick = function() {
         payment - payment.toFixed(2);
 
         //step 3: display results
+        let text = "";
+        text += "Loan: " + principal.toFixed(2);
+        text += "Total Interest: " + totalInterest,toFixed(2);
+        text += "Total Cost: " + totalLoan.toFixed(2);
+        text += "Monthly Payment: " + payment.toFixed(2);
 
     }
 
     catch (error) {
-        alert("Something went wrong");
+        alert(error);
     }
 }
